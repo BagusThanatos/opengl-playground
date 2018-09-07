0ba00f1fb502d09ba38acbad75d016e6b1042e04
Bagus Nurtomo <bagusthanatos@gmail.com>
2018-09-07 07:20:49 +0700

    Build to x86 for now, CHANGE TO X64 LATER. Still not working tho

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 IF NOT EXIST build\windows mkdir build\windows
 pushd build\windows
-cl -I ..\..\include\ -Zi ..\..\src\main.cpp user32.lib gdi32.lib /link /incremental:no /LIBPATH:..\..\lib\
+cl -I ..\..\include\ -Zi ..\..\src\main.cpp ..\..\src\shaders.cpp  /EHsc /link /incremental:no /LIBPATH:..\..\lib\ glew32.lib FreeImage.lib glew32s.lib glut32.lib
 popd
