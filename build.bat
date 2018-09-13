25d06e9f346a57f719cc0eeeedaefc07f9bee57b
Bagus Nurtomo <bagusthanatos@gmail.com>
2018-09-13 07:51:59 +0700

    Trying to init, should probably just go to the plain way

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 IF NOT EXIST build\windows mkdir build\windows
 pushd build\windows
-cl -I ..\..\include\ -Zi ..\..\src\main.cpp ..\..\src\shaders.cpp  /EHsc /link /incremental:no /LIBPATH:..\..\lib\ glew32.lib FreeImage.lib glew32s.lib glut32.lib
+cl -I ..\..\include\ -Zi ..\..\src\main.cpp ..\..\src\shaders.cpp  /EHsc /link /incremental:no /LIBPATH:..\..\lib\ glew32s.lib glew32.lib glut32.lib FreeImage.lib 
 popd
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
+#define GLEW_STATIC
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <GL/glew.h>
 #include <GL/glut.h>
 
-int main()
+
+
+int main(int argc, char** argv)
 {
+    glutInit(&argc, argv);
+    GLenum err = glewInit();
+    
+    if(GLEW_OK != err) {
+        printf("ERROR OCCURED!!!!\n");
+        return 1;
+    }
+    
+    int version;
+    glGetIntegerv(GL_MAJOR_VERSION, &version);
+    const char* sversion = (const char*) glGetString(GL_VERSION);
+    
+    printf("%d\n", version);
+    printf("%s", sversion);
+    
     printf("coba\n");
     return 0;
 }
+
